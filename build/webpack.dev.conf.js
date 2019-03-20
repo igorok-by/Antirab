f0ffaf12c076c5bd60c0fafb7ba2219a52964be6
Igor B <igorko.by@gmail.com>
2019-03-20 18:54:58 +0300

    Adding blocks; adjust html

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  // DEV settings gonna be here
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: baseWebpackConfig.externals.paths.dist,
+    // historyApiFallback: true,
+    // noInfo: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    port: 8081,
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[file].map"
+    })
+  ]
+})
+
+// export devWebpackConfig
+module.exports = new Promise((resolve, reject) => {
+ resolve(devWebpackConfig)
+})
